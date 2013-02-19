a68055314c81e738f4a148e0a395994dc861df52
Andrew Kostka <akostka@gunnjerkens.com>
2013-02-19 15:11:24 -0800

    Added plugins

--- wp-content/themes/gj-boilerplate/inc/assets.php
+++ wp-content/themes/gj-boilerplate/inc/assets.php
@@ -7,11 +7,15 @@ function enqueue_scripts() {
 	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', false, null, true);
 	wp_enqueue_script('jquery');
 
+	wp_enqueue_style('fancybox', get_bloginfo('template_directory') . '/style/fancybox/jquery.fancybox.css', false, null, false);
 	wp_enqueue_style('custom_style', get_bloginfo('template_directory') . '/style/style.css', false, null, false);
 
 	wp_enqueue_script('modernizr', get_bloginfo('template_directory') . '/js/libs/modernizr.js', false, null, false);
 	wp_enqueue_script('plugins', get_bloginfo('template_directory') . '/js/plugins.js', false, null, true);
-	wp_enqueue_script('custom', get_bloginfo('template_directory') . '/js/script.js', false, null, true);
+	wp_enqueue_script('cycle', get_bloginfo('template_directory') . '/js/libs/jquery.cycle.js', array('jquery'), null, true);
+	wp_enqueue_script('fancybox', get_bloginfo('template_directory') . '/js/libs/jquery.fancybox.pack.js', array('jquery'), null, true);
+	wp_enqueue_script('placeholder', get_bloginfo('template_directory') . '/js/libs/jquery.placeholder.min.js', array('jquery'), null, true);
+	wp_enqueue_script('custom', get_bloginfo('template_directory') . '/js/script.js', array('jquery'), null, true);
 
 }
 
--- wp-content/themes/gj-boilerplate/js/script.js
+++ wp-content/themes/gj-boilerplate/js/script.js
@@ -1,8 +1,6 @@
-/* Author:
+jQuery(document).ready(function($) {
 
-*/
+	$('input, textarea').placeholder();
+	$('.fancybox').fancybox();
 
-
-
-
-
+});
