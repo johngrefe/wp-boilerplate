8da75e5a192fb4d37c2b62aa79f9a52a2e4a3787
Kevin Crawford <kcrawford@gunnjerkens.com>
2013-04-10 15:54:35 -0700

    Minor reformatting for compass config

--- wp-content/themes/gj-boilerplate/style/config.rb
+++ wp-content/themes/gj-boilerplate/style/config.rb
@@ -2,19 +2,17 @@
 require 'compass-normalize'
 
 # Set this to the root of your project when deployed:
-http_path = "/wp-content/themes/brookfieldsocal/style/"
+http_path = "/wp-content/themes/boilerplate/style/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "../img"
 javascripts_dir = "../js"
 
 generated_images_dir = images_dir + "/generated"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
