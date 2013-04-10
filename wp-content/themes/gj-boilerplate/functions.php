64c17067fb385ee32bcb96d30922c882cba88de2
Kevin Crawford <kcrawford@gunnjerkens.com>
2013-04-10 15:50:15 -0700

    Break out some theme initialization methods from functions.php -> inc/init.php. Closes #11

--- wp-content/themes/gj-boilerplate/functions.php
+++ wp-content/themes/gj-boilerplate/functions.php
@@ -1,10 +1,7 @@
 <?php
 
 include('inc/assets.php');
-
-// Define upload_url_path to embed media with domain-relative URLs
-// Only needs to be excuted once on project initialization
-update_option('upload_url_path', '/wp-content/uploads');
+include('inc/init.php')
 
 if( WP_PASSWORD_PROTECT == true ){
 	
---
+++ wp-content/themes/gj-boilerplate/inc/init.php
@@ -0,0 +1,5 @@
+<?php
+
+// Define upload_url_path to embed media with domain-relative URLs
+// Only needs to be excuted once on project initialization
+update_option('upload_url_path', '/wp-content/uploads');
